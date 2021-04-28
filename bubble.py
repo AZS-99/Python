da2e049983f1a29ad99ed341f8c53276d2889587
adam <sadam4@myseneca.ca>
2021-04-28 08:32:59 -0400

    prune

--- bubble.py
+++
@@ -1,9 +0,0 @@
-def bubble_sort(lst: list) -> list:
-    length = len(lst)
-    for i in range(0, length):
-        element = lst[i]
-        index = i
-        for j in range (i + 1, length):
-            if lst[j] < lst[i]:
-                lst[j], lst[i] = lst[i], lst[j]
-    return lst
