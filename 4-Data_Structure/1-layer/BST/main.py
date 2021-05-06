827077393947d2cfdeb198811e6418244985ad65
adam <sadam4@myseneca.ca>
2021-05-06 15:52:38 -0400

    BST->remove_root()

--- 4-Data_Structure/1-layer/BST/main.py
+++ 4-Data_Structure/1-layer/BST/main.py
@@ -3,8 +3,13 @@ from node import Node
 if __name__ == '__main__':
     n = Node(10)
     print(n.insert(11))
-    print(n.insert(9))
+    print(n.insert(7))
     print(n.insert(0))
     print(n.insert(20))
+    n.insert(8)
+    n.insert(30)
+    n.insert(9)
 
-    print(len(n))
+    print(n)
+    n.remove_root()
+    print(n)
--- 4-Data_Structure/1-layer/BST/node.py
+++ 4-Data_Structure/1-layer/BST/node.py
@@ -17,6 +17,14 @@ class Node:
             return 1
         return (len(self.left) if self.left else 0) + (len(self.right) if self.right else 0) + 1
 
+    def find_far_right_leaf(self):
+        if not self.right:
+            return None, self
+        elif not self.right.right:
+            return self, self.right
+        else:
+            return self.right.find_far_right_leaf()
+
     # Note that Python pointers are all of type (void*), so once self.left/self.right is None, its pointer doesn't hold
     # the type "Node", and the interpreter wouldn't know what "height" to look into, so we need the ternary
     def height(self):
@@ -45,6 +53,18 @@ class Node:
         return (self.left.print_level(level - 1) if self.left else "\u03C6") + " " + \
                (self.right.print_level(level - 1) if self.right else "\u03C6")
 
+    def remove_root(self):
+        if not self.left:
+            parent, node = self.find_far_right_leaf()
+            node.value, self.value = self.value, node.value
+            parent.right = None
+        elif not self.left.right:
+            self.left.value, self.value = self.value, self.left.value
+            self.left = self.left.left
+        else:
+            parent, node = self.left.find_far_right_leaf()
+            node.value, self.value = self.value, node.value
+            parent.right = None
 
 
 
